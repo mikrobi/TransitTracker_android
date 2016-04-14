cbec8941a0a14c9d3bfa4623cd5aed8dcb02e6c5
Jakob Class <jakob.class@gmail.com>
2016-04-14 18:50:23 +0200

    Kindly ask for location permission.

--- app/src/main/java/de/jakobclass/transittracker/MapActivity.kt
+++ app/src/main/java/de/jakobclass/transittracker/MapActivity.kt
@@ -1,18 +1,25 @@
 package de.jakobclass.transittracker
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.support.v4.app.FragmentActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 class MapActivity : FragmentActivity(), OnMapReadyCallback {
 
     private var map: GoogleMap? = null
+
+    private val REQUEST_CODE_ACCESS_FINE_LOCATION = 1
+    private val REQUIRED_LOCATION_PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +33,37 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
 
         val berlinCityCenter = LatLng(52.520048, 13.404773)
         map!!.moveCamera(CameraUpdateFactory.newLatLngZoom(berlinCityCenter, 16.0f))
+        initLocationPermission()
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_ACCESS_FINE_LOCATION && permissions.isNotEmpty()
+                && permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            enableMyLocation()
+        }
+    }
+
+    private fun initLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, REQUIRED_LOCATION_PERMISSION)
+                == PackageManager.PERMISSION_GRANTED) {
+            enableMyLocation()
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_LOCATION_PERMISSION)) {
+            AlertDialog.Builder(this).setMessage(R.string.location_usage_description)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setPositiveButton(R.string.ok) { dialog, index -> requestLocationPermission() }
+                    .show()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(REQUIRED_LOCATION_PERMISSION), REQUEST_CODE_ACCESS_FINE_LOCATION)
+    }
+
+    private fun enableMyLocation() {
+        map?.isMyLocationEnabled = true
+    }
+
 }
