6dc8d755a643fe2265b32710e91394c75270e035
Jakob Class <jakob.class@gmail.com>
2016-04-14 12:45:48 +0200

    Initially center map to Berlin city center.
    Closes #1

--- app/src/main/java/de/jakobclass/transittracker/MapActivity.kt
+++ app/src/main/java/de/jakobclass/transittracker/MapActivity.kt
@@ -17,27 +17,14 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map!!.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map!!.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val berlinCityCenter = LatLng(52.520048, 13.404773)
+        map!!.moveCamera(CameraUpdateFactory.newLatLngZoom(berlinCityCenter, 16.0f))
     }
 }
